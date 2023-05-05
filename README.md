95a702fbedcd1233ca9187f975eb30c0b40d8185
JorgeGarcia-Dev <georgegarciacode@outlook.com>
2023-05-05 17:20:39 -0600

    Primer commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Calculadora Evolutiva
+
+---
+
+En este programa hemos desarrollado una calculadora, que se ha cosntruido en base a funciones,
+mismas que se van ejecutando en base a las necesidades del usuario, el cual puede elegir entre
+las siguientes opciones:
+
+1. Suma
+2. Resta
+3. Multiplicación
+4. División
+
+Al ejecutar el programa, se despliega un menú, en el cual se le pide al usuario que ingrese
+el número de la opción que desea ejecutar, posteriormente se le pide que ingrese los números
+con los que desea realizar la operación, y finalmente se despliega el resultado de la operación
+que el usuario haya elegido.
+
+---
+
+El programa fue desarrollado con el lenguaje de programación C#, y se utilizó el IDE Visual Studio Code.
+
+---
+
+Para correr el programa se debe ejecutar el siguiente comando:
+
+```dotnet run```
+
+Esto ya sea desde la terminal de Visual Studio Code, o desde la terminal de Windows/Mac/Linux.
---
+++ calculadoraEvolutiva/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace calculadoraEvolutiva
+{
+    class Program
+    {
+        static void Sumar()
+        {
+            System.Console.WriteLine("Ingresa el primer número para tu operación:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            System.Console.WriteLine("Ingresa el segundo número para tu operación:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int resultado = num1 + num2;
+            System.Console.WriteLine($"El resultado de la suma es: {resultado}");
+        }
+
+        static void Restar()
+        {
+            System.Console.WriteLine("Ingresa el primer número para tu operación:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            System.Console.WriteLine("Ingresa el segundo número para tu operación:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int resultado = num1 - num2;
+            System.Console.WriteLine($"El resultado de la resta es: {resultado}");
+        }
+
+        static void Multiplicar()
+        {
+            System.Console.WriteLine("Ingresa el primer número para tu operación:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            System.Console.WriteLine("Ingresa el segundo número para tu operación:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int resultado = num1 * num2;
+            System.Console.WriteLine($"El resultado de la multilicación es: {resultado}");
+        }
+
+        static void Dividir()
+        {
+            System.Console.WriteLine("Ingresa el primer número para tu operación:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            System.Console.WriteLine("Ingresa el segundo número para tu operación:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int resultado = num1 / num2;
+            System.Console.WriteLine($"El resultado de la división es: {resultado}");
+        }
+
+        static void Calculadora()
+        {
+            bool fin = false;
+            int opcion = 0;
+
+            while(!fin)
+            {
+                System.Console.WriteLine("Opción: ");
+                opcion = Convert.ToInt32(Console.ReadLine());
+                switch(opcion)
+                {
+                    case 1:
+                        Sumar();
+                        break;
+                    case 2:
+                        Restar();
+                        break;
+                    case 3:
+                        Multiplicar();
+                        break;
+                    case 4:
+                        Dividir();
+                        break;
+                    case 5:
+                        fin = true;
+                        break;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("********** Calculadora Evolutiva **********");
+            System.Console.WriteLine("Menú:");
+            System.Console.WriteLine("1. Sumar");
+            System.Console.WriteLine("2. Restar");
+            System.Console.WriteLine("3. Multiplicar");
+            System.Console.WriteLine("4. Dividir");
+            System.Console.WriteLine("5. Salir");
+            Calculadora();
+            System.Console.WriteLine("Programa Finalizado.");
+            Console.ReadLine();
+        }
+    }
+}
